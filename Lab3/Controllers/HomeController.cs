1ff3aa82fcc806d5c543721130883a825265d773
Meepoloy <godswintorres@yahoo.com>
2023-10-15 21:13:22 -0400

    Reupload

---
+++ Lab3/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Lab3.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult SongForm() => View();
+
+        [HttpPost]
+        public IActionResult Sing()
+        {
+            HttpContext.Session.SetString("inputNum", Request.Form["inputNum"]);
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateStudent() => View();
+
+        [HttpPost]
+        public IActionResult DisplayStudent(Student model)
+        {
+            // you will complete this
+            return View(model);
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab3/Model/Student.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab3.Model
+{
+    public class Student
+    {
+        public string firstName
+        {
+            get;
+            set;
+        }
+
+        public string lastName
+        {
+            get;
+            set;
+        }
+        public string studId
+        {
+            get;
+            set;
+        }
+        public string emailAdd
+        {
+            get;
+            set;
+        }
+        public string password
+        {
+            get;
+            set;
+        }
+        public string description
+        {
+            get;
+            set;
+        }
+
+    }
+}
